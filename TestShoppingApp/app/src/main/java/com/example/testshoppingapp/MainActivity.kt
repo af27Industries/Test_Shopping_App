2cf92ae42a926f1ebe76b6db40501106234ed1ad
af27industries <af27industries@outlook.com>
2023-01-23 13:47:39 -0500

    feat(*): Splash screen
    - add library for supporting splash screen
    - add icon of the app
    - add splash screen style theme
    
    #US-2

--- TestShoppingApp/app/src/main/java/com/example/testshoppingapp/MainActivity.kt
+++ TestShoppingApp/app/src/main/java/com/example/testshoppingapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.testshoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 class MainActivity : AppCompatActivity() {
+
+    var contentHasLoaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        startLoadingContent()
+        setupSplashScreen()
+
+
+    }
+
+    private fun startLoadingContent() {
+        Timer().schedule(3000){
+            contentHasLoaded = true
+        }
+    }
+
+    private fun setupSplashScreen() {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (contentHasLoaded) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else false
+                }
+            }
+        )
     }
 }
